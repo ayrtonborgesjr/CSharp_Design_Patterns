36d8061e619a8aeffdd0bec1894fcddb412bc206
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2020-05-15 20:42:25 -0300

    Builder

---
+++ 04-Builder/CellPhone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _04_Builder
+{
+    public class CellPhone
+    {
+        public CellPhone(string mobileName)
+        {
+            this.Name = mobileName;
+        }
+
+        public string MobileName;
+        public string Name { get; set; }
+        public string Screen { get; set; }
+        public string Battery { get; set; }
+        public string OperationaSystem { get; set; }
+        public string Camera { get; set; }
+    }
+}
---
+++ 04-Builder/ICellPhone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04_Builder
+{
+    public interface ICellPhone
+    {
+        void BuildScreen();
+        void BuildBattery();
+        void BuildOperationalSystem();
+        void BuildCamera();
+        CellPhone CellPhone { get; }
+
+
+    }
+}
