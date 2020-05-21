00b856e45e40bdfb7780902c5f8a9d0a8cfa45a6
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2020-05-21 14:12:36 -0300

    Template Method

---
+++ Template Method/AbstractClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Template_Method
+{
+    public abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        
+        public abstract void PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine();
+        }
+
+    }
+}
--- Template Method/Program.cs
+++ Template Method/Program.cs
@@ -6,7 +6,16 @@ namespace Template_Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Template Method");
+            Console.WriteLine();
+
+            AbstractClass abstractClassA = new ConcreteClassA();
+            abstractClassA.TemplateMethod();
+
+            AbstractClass abstractClassB = new ConcreteClassB();
+            abstractClassB.TemplateMethod();
+
+            Console.ReadKey();
         }
     }
 }
