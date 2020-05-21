42eae25e368cc688c1792b1f246a256877173bef
ayrtonborgesjr <ayrton.borges@yahoo.com.br>
2020-05-21 14:42:43 -0300

    Visitor

---
+++ Visitor/ObjectStructure.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Visitor
+{
+    public class ObjectStructure
+    {
+        private List<Element> Elements = new List<Element>();
+
+        public void Attach(Element element)
+        {
+            this.Elements.Add(element);
+        }
+
+        public void Detach(Element element)
+        {
+            this.Elements.Remove(element);
+        }
+
+        public void Accept(Visitor visitor)
+        {
+            foreach (Element item in Elements)
+            {
+                item.Accept(visitor);
+            }
+        }
+    }
+}
--- Visitor/Program.cs
+++ Visitor/Program.cs
@@ -6,7 +6,20 @@ namespace Visitor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Visitor");
+            Console.WriteLine();
+
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new ConcreteElementA());
+            o.Attach(new ConcreteElementB());
+
+            ConcreteVisitor1 v1 = new ConcreteVisitor1();
+            ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+            o.Accept(v1);
+            o.Accept(v2);
+
+            Console.ReadKey();
         }
     }
 }
